e3e26497c0a09a2b724eedbbded79dbadd6c3181
mysticmind <babu.annamalai@gmail.com>
2015-07-16 14:12:10 +0530

    Further fixes to check and remove newline+tab in varying environments

--- src/ReverseMarkdown/Cleaner.cs
+++ src/ReverseMarkdown/Cleaner.cs
@@ -11,7 +11,9 @@ namespace ReverseMarkdown
 		private string CleanTagBorders(string content)
 		{
 			// content from some htl editors such as CKEditor emits newline and tab between tags, clean that up
-			return content.Replace("\n\t", "");
+			content = content.Replace("\n\t", "");
+			content = content.Replace(Environment.NewLine + "\t", "");
+			return content;
 		}
 
 		public string PreTidy(string content)
