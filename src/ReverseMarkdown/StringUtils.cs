903813cb4ee2e600b286a7cbb2f57054529191f4
mysticmind <babu.annamalai@gmail.com>
2019-01-24 16:04:36 +0530

    Move method GetScheme to StringUtils and remove LinkParser

--- src/ReverseMarkdown/StringUtils.cs
+++ src/ReverseMarkdown/StringUtils.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ReverseMarkdown
 {
@@ -16,6 +18,32 @@ namespace ReverseMarkdown
             using (var sr = new StringReader(content))
                 while ((line = sr.ReadLine()) != null)
                     yield return line;
+        }
+        
+        /// <summary>
+        /// <para>Gets scheme for provided uri string to overcome different behavior between windows/linux. https://github.com/dotnet/corefx/issues/1745</para>
+        /// Assume http for url starting with //
+        /// <para>Assume file for url starting with /</para>
+        /// Otherwise give what <see cref="Uri.Scheme" /> gives us.
+        /// <para>If non parseable by Uri, return empty string. Will never return null</para>
+        /// </summary>
+        /// <returns></returns>
+        internal static string GetScheme(string url) {
+            var isValidUri = Uri.TryCreate(url, UriKind.Absolute, out Uri uri);
+            //IETF RFC 3986 
+            if (Regex.IsMatch(url, "^//[^/]")) {
+                return "http";
+            }
+            //Unix style path
+            else if (Regex.IsMatch(url, "^/[^/]")) {
+                return "file";
+            } 
+            else if (isValidUri) {
+                return uri.Scheme;
+            }
+            else {
+                return String.Empty;
+            }
         }
     }
 }
