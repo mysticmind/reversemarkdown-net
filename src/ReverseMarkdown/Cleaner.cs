0b6c291599abe7dcdca633739504520454b44371
mysticmind <babu.annamalai@gmail.com>
2019-03-28 11:13:09 +0530

    Update claner to normalize space chars
    - Update claner to normalize unicode and non-breaking spaces to normal
    space
    - Update unit tests

--- src/ReverseMarkdown/Cleaner.cs
+++ src/ReverseMarkdown/Cleaner.cs
@@ -21,8 +21,16 @@ namespace ReverseMarkdown
             return content;
         }
 
+        private static string NormalizeSpaceChars(string content)
+        {
+            // replace unicode and non-breaking spaces to normal space
+            content = Regex.Replace(content, @"[\u0020\u00A0]", " ");
+            return content;
+        }
+
         public static string PreTidy(string content, bool removeComments)
         {
+            content = NormalizeSpaceChars(content);
             content = CleanTagBorders(content);
 
             if (removeComments)
