2e9a84ed07805c87a5beb6ff2f80d0d4d9c4cc87
Simon Cropp <simon.cropp@gmail.com>
2020-08-05 20:40:19 +1000

    Add verify and mdsnippets usage (#73)
    * Add verify and mdsnippets
    * Update build
    * Update ReverseMarkdown.csproj
    * Update .travis.yml
    * Update ReverseMarkdown.csproj

---
+++ src/ReverseMarkdown.Test/Snippets.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using ReverseMarkdown;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public class Snippets
+{
+    [Fact]
+    public async Task Usage()
+    {
+        #region Usage
+
+        var converter = new ReverseMarkdown.Converter();
+
+        string html = "This a sample <strong>paragraph</strong> from <a href=\"http://test.com\">my site</a>";
+
+        string result = converter.Convert(html);
+
+        #endregion
+
+        await Verifier.Verify(result);
+    }
+
+    [Fact]
+    public void UsageWithConfig()
+    {
+        #region UsageWithConfig
+
+        var config = new ReverseMarkdown.Config
+        {
+            // Include the unknown tag completely in the result (default as well)
+            UnknownTags = Config.UnknownTagsOption.PassThrough,
+            // generate GitHub flavoured markdown, supported for BR, PRE and table tags
+            GithubFlavored = true,
+            // will ignore all comments
+            RemoveComments = true,
+            // remove markdown output for links where appropriate
+            SmartHrefHandling = true
+        };
+
+        var converter = new ReverseMarkdown.Converter(config);
+
+        #endregion
+    }
+}
